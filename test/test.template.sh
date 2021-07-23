3081b43981362d95ee9ad26e6a5917688230e0bb
robertjahn <rob.jahn@gmail.com>
2021-07-23 14:07:31 -0400

    adjust to use new library functions

--- test/test.template.sh
+++ test/test.template.sh
@@ -1,17 +1,22 @@
+#!/bin/bash
+
+# read in the test library
+source test-library.sh
+
+# adjust values for your testing needs
 export KEPTN_BASE_URL=https://XXXXX.cloudautomation.live.dynatrace.com
 export KEPTN_BRIDGE_URL=https://XXXXX.cloudautomation.live.dynatrace.com
 export KEPTN_API_TOKEN=
-
 export DT_BASE_URL=https://XXXXX.live.dynatrace.com
 export DT_API_TOKEN=
-
-export IMAGE=dtdemos/keptn-automation:1
-
-cd .. && ./buildpush.sh && cd test/
+export IMAGE=dtdemos/keptn-automation:0.1.0
 
 # uncomment test as required
 
-#./test-createdynatracesecret.sh
+# optionally build as part of testing
+#cd .. && ./buildpush.sh && cd test/
+
+#createdynatracesecret
 
 export MOUNT=/keptn-mount
 export KEPTN_SHIPYARD_FILE=$MOUNT/shipyard.yaml
@@ -19,7 +24,7 @@ export KEPTN_PROJECT=dt-orders
 export KEPTN_SERVICE=catalog
 export KEPTN_STAGE=dev
 export DEBUG=true
-#./test-onboardservice.sh
+#createservice
 
 export MOUNT=/keptn-mount
 export KEPTN_SHIPYARD_FILE=$MOUNT/shipyard.yaml
@@ -27,24 +32,23 @@ export KEPTN_PROJECT=dt-orders
 export KEPTN_SERVICE=catalog
 export KEPTN_STAGE=dev
 export DEBUG=true
-export CONFIGURE_DT_MONITORING=true
+export CONFIGURE_DT_PROVIDER=true
 export DT_CONF_FILE=$MOUNT/dynatrace.conf.yaml
-./test-onboardservice-with-monitoring.sh
+#createservice-with-configure-dt-provider
+
+#createservice-with-upstream
 
 export EVALUATION_RULE=pass_on_warning     # e.g. ignore, pass_on_warning, fail_on_warning
 export KEPTN_PROJECT=dt-orders
 export KEPTN_SERVICE=catalog
 export KEPTN_STAGE=dev
-#export KEPTN_PROJECT=dynatrace
-#export KEPTN_SERVICE==frontend
-#export KEPTN_STAGE=quality-gate
 #export LABELS=buildId=1,executedBy=manual​
 export LABELS=runId=939511494,executedBy=GitHub,Job=https://github.com/dt-demos/github-actions/actions/runs/939511494
 #export START=2021-06-09T21:00:00
 #export END=2021-06-09T22:00:00
 export TIMEFRAME=5m
 export DEBUG=true
-#./test-sloevaluation.sh
+#sloevaluation
 
 export KEPTN_COMMAND="get project"
-#./test-keptn.sh
+#keptn
